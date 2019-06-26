3df25e1d0d456c10a717dce8076947faa3cc12f4
Luke Nguyen <lukehaonguyen@gmail.com>
2019-06-26 09:40:17 -0600

    redux dev tools in store change

--- client/src/store.js
+++ client/src/store.js
@@ -6,19 +6,19 @@ const initialState = {};
 
 const middleware = [thunk];
 
-const store = createStore(
-  rootReducer,
-  initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
 // const store = createStore(
 //   rootReducer,
 //   initialState,
-//   compose(applyMiddleware(...middleware))
+//   compose(
+//     applyMiddleware(...middleware),
+//     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+//   )
 // );
+const store = createStore(
+  rootReducer,
+  initialState,
+  compose(applyMiddleware(...middleware))
+);
 
 // if(process.env.NODE_ENV === 'production') {
 //   store = createStore(rootReducer, initialState, compose(
